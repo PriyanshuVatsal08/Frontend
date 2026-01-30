b716d20c74b2a80962105d479f0f244239cbbd77
PriyanshuVatsal8 <priyanshuvatsal2572004@gmai.com>
2026-01-30 15:35:18 +0530

    ES6 complete

---
+++ JavaScript Folder/ES6_Features/script.js
@@ -0,0 +1,23 @@
+// //let and const
+
+try {
+    console.log(age);
+} catch (error) {
+    console.log(error);
+}
+let age=23;
+
+// //Default function & Arrow function
+let sum=(a,b=10)=>{
+    return a+b;
+}
+
+console.log(sum(15));
+
+
+//Template string
+let myName="Priyanshu Vatsal";
+
+console.log(`Hello ${myName}, Who are you?`);
+
+
