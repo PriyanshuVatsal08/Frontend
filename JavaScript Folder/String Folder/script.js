5d6a1aa019246ee182d1dde43618e273a6fde916
PriyanshuVatsal8 <priyanshuvatsal2572004@gmail.com>
2026-02-01 21:28:04 +0530

    String is done with method

---
+++ JavaScript Folder/String Folder/script.js
@@ -0,0 +1,27 @@
+console.log(`Hello I am "Priyanshu Vatsal", 'full stack devloper' `)
+
+//Case conversion
+let str="Hello World World";
+console.log(str.toLowerCase());
+
+//Searching Method
+
+//1). includes();
+console.log(str.includes("Hello"));
+
+//2). indexOf();
+console.log(str.indexOf("World"));
+
+//3). lastIndexOf();
+console.log(str.lastIndexOf("World"));
+
+let pair=str.slice(1,7);
+console.log(pair);
+console.log(str);
+
+console.log(str.replaceAll("World","Sansar"));
+
+
+//repeat()
+console.log("ha".repeat(3));
+
