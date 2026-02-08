0a44eb03c9bb01e48c7603aead6f704cf2f16ba2
PriyanshuVatsal8 <priyanshuvatsal2572004@gmail.com>
2026-02-08 15:23:36 +0530

    Tribute page created

---
+++ JavaScript Folder/Events and Listeners/script.js
@@ -0,0 +1,16 @@
+// function change(){
+//     let heading=document.getElementById('heading');
+//     heading.innerHTML="Hello Priyanshu Vatsal";
+// }
+
+// let button=document.getElementById('button');
+
+
+// button.addEventListener("click", change);
+
+let box=document.getElementById('box');
+
+box.addEventListener("click",()=>{
+    box.classList.toggle('circle');
+});
+
