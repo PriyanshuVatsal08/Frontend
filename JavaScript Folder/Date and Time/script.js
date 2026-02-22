e0c2e4ddb2d6d146a0468a6b0af449a2146db61c
PriyanshuVatsal08 <priyanshuvatsal2572004@gmail.com>
2026-02-22 11:34:57 +0530

    Watch is complete

--- JavaScript Folder/Date and Time/script.js
+++ JavaScript Folder/Date and Time/script.js
@@ -1,41 +1,50 @@
 // Get date and time
-let d=new Date();
-console.log(d);
-console.log(d.getFullYear());
-console.log(d.getHours()+" "+d.getMinutes()+" "+d.getSeconds());
+// let d=new Date();
+// console.log(d);
+// console.log(d.getFullYear());
+// console.log(d.getHours()+" "+d.getMinutes()+" "+d.getSeconds());
 
-// Set date and time
-let d=new Date();
-d.setDate(5);
-console.log(d);
+// // Set date and time
+// let d=new Date();
+// d.setDate(5);
+// console.log(d);
 
-d.setFullYear(2028);
-console.log(d);
+// d.setFullYear(2028);
+// console.log(d);
 
-toLocalString
-let d=new Date();
-console.log(d.toLocaleString());
-console.log(d.toLocaleDateString());
-console.log(d.toLocaleTimeString());
+// toLocalString
+// let d=new Date();
+// console.log(d.toLocaleString());
+// console.log(d.toLocaleDateString());
+// console.log(d.toLocaleTimeString());
 
 
-//Ques:- Add date in alreday given date
-const addDate=(date,extraDay)=>{
-    let x=date.setDate(date.getDate() + extraDay);
-    return new Date(x);
+// //Ques:- Add date in alreday given date
+// const addDate=(date,extraDay)=>{
+//     let x=date.setDate(date.getDate() + extraDay);
+//     return new Date(x);
+// }
+// let date=new Date("2024-02-29");
+// console.log(addDate(date,7).toLocaleString());
+
+
+// // Ques :- Print a line for 1 min and stop after that
+
+// function print() {
+//    console.log("Function call after every 1 sec");
+// }
+
+// let id=setInterval(print, 1000);
+
+// setTimeout(() => {
+//    clearInterval(id);
+// }, 5000);
+
+
+function createTimer(){
+   let time=new Date().toLocaleTimeString();
+   let timerDiv=document.querySelector("#timeDiv");
+   timerDiv.innerText=time;
 }
-let date=new Date("2024-02-29");
-console.log(addDate(date,7).toLocaleString());
 
-
-// Ques :- Print a line for 1 min and stop after that
-
-function print() {
-   console.log("Function call after every 1 sec");
-}
-
-let id=setInterval(print, 1000);
-
-setTimeout(() => {
-   clearInterval(id);
-}, 5000);
+let intervelId=setInterval(createTimer,1000);
